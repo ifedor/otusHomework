027a9519ec0c37a9c16b8ef0b0413dd20e97caba
ifedorchuk <ifedorchuk@yandex-team.ru>
2022-05-10 15:52:30 +0300

    CustomerReverseOrder linkedList type hinting changed to Deque interface

--- hw04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,9 +1,10 @@
 package homework;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
-    private final LinkedList<Customer> linkedList;
+    private final Deque<Customer> linkedList;
 
     public CustomerReverseOrder() {
         this.linkedList = new LinkedList<>();
